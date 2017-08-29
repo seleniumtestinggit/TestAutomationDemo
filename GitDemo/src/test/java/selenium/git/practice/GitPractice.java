0edd150ba009aa0f5ffc741204437df229a330b8
Nijazi <Nijazi@LAPTOP-2K0PQNQS.home>
2017-08-28 22:40:10 -0400

    Auto -117: Adding new automation project

---
+++ GitDemo/src/test/java/selenium/git/practice/GitPractice.java
@@ -0,0 +1,19 @@
+package selenium.git.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GitPractice {
+
+	WebDriver driver;
+
+	@Test
+	public void openBrowser()
+	{
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/");
+
+	}
+}
